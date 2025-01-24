bddb79aa36ae9b9f3a3cb8ff7b05f5e9ce74f2c5
gugumeow <gugumeow2009@gmail.com>
2025-01-24 16:16:56 +0800

    202501241616

--- kernel.c
+++ kernel.c
@@ -99,6 +99,7 @@ typedef unsigned int uint32_t;
 typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
+extern char __free_ram[], __free_ram_end[];
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
@@ -123,6 +124,18 @@ void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
+paddr_t alloc_pages(uint32_t n) {
+    static paddr_t next_paddr = (paddr_t) __free_ram;
+    paddr_t paddr = next_paddr;
+    next_paddr += n * PAGE_SIZE;
+
+    if (next_paddr > (paddr_t) __free_ram_end)
+        PANIC("out of memory");
+
+    memset((void *) paddr, 0, n * PAGE_SIZE);
+    return paddr;
+}
+
 
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
@@ -134,6 +147,11 @@ void kernel_main(void) {
 
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+
+    paddr_t paddr0 = alloc_pages(2);
+    paddr_t paddr1 = alloc_pages(1);
+    printf("alloc_pages test: paddr0=%x\n", paddr0);
+    printf("alloc_pages test: paddr1=%x\n", paddr1);
     
     PANIC("booted!");
     printf("unreachable here!\n");
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ QEMU=qemu-system-riscv32
 
 # Path to clang and compiler flags
 CC=clang  # Ubuntu users: use CC=clang, macOS users: CC=/opt/homebrew/opt/llvm/bin/clang
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O0 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # Build the kernel
 # $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c

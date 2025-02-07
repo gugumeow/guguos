7b79516b1b3e1c0e28846a04ec8fb5525c2ce987
gugumeow <gugumeow2009@gmail.com>
2025-02-07 21:00:56 +0800

    202502072100

---
+++ 09/common.c
@@ -0,0 +1,104 @@
+// 05
+#include "common.h"
+
+// 06
+void *memset(void *buf, char c, size_t n) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+// 06
+void *memcpy(void *dst, const void *src, size_t n) {
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+// 06
+char *strcpy(char *dst, const char *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+// 06
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
+// 05
+void putchar(char ch);
+
+// 05
+void printf(const char *fmt, ...) {
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++; // 跳过 '%'
+            switch (*fmt) { // 读取下一个字符
+                case '\0': // 当 '%' 作为格式字符串的末尾
+                    putchar('%');
+                    goto end;
+                case '%': // 打印 '%'
+                    putchar('%');
+                    break;
+                case 's': { // 打印以 NULL 结尾的字符串
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+                case 'd': { // 以十进制打印整型
+                    int value = va_arg(vargs, int);
+                    if (value < 0) {
+                        putchar('-');
+                        value = -value;
+                    }
+
+                    int divisor = 1;
+                    while (value / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor > 0) {
+                        putchar('0' + value / divisor);
+                        value %= divisor;
+                        divisor /= 10;
+                    }
+
+                    break;
+                }
+                case 'x': { // 以十六进制打印整型
+                    int value = va_arg(vargs, int);
+                    for (int i = 7; i >= 0; i--) {
+                        int nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
---
+++ 09/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xue
+
+# QEMU 啟動路徑
+QEMU=qemu-system-riscv32
+
+# clang 路徑和編譯器標誌
+# macOS 用戶：使用 CC=/opt/homebrew/opt/llvm/bin/clang  # Ubuntu 用户：使用 CC=clang
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# 構建內核
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
+
+# 啟動 QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf 
